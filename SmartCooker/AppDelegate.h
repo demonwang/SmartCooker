23bbdff6ebbf7187c6f9ade12019927fd15cad84
Playboy王某某 <wm5821090@gmail.com>
2015-04-21 21:21:37 +0800

    SmartCooker
    LuanApp

---
+++ SmartCooker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartCooker
+//
+//  Created by wangmeng on 15/4/21.
+//  Copyright (c) 2015年 HF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartCooker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartCooker
+//
+//  Created by wangmeng on 15/4/21.
+//  Copyright (c) 2015年 HF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
